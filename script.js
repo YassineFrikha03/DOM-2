cb602918defd7e88e6dd4cf3d7d6910be67b578c
“Yassine <yassinefrikha0@gmail.com>
2025-02-27 20:43:37 +0100

    dom 2

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-
-
-let button =document.getElementById('button')
+let button = document.getElementById("change-color-btn")
 function getRandomColor() {
-    const chiffreHexa = '0123456789ABCDEF'
-    let color = '#'
-
+    const chiffreHexa = '0123456789ABCEF'
+    let color = "#"
 
     for (let i = 0; i < 6; i++) {
         let randomNum = Math.floor(Math.random()*15)
-        console.log(randomNum)
         color = color + chiffreHexa[randomNum]
     }
     return color
 }
 
-
-button.addEventListener ("click", function () {
- const colorBox = document.getElementById('colorBox')
- colorBox.style.backgroundColor = getRandomColor()
+button.addEventListener("click", function () {
+    const colorBox = document.getElementById('color-box')
+    colorBox.style.backgroundColor = getRandomColor()
 })
